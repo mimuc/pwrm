cd07e461fdb955bfe8094452ae39fa2c39718df3
Fuasmattn <mprinz90@googlemail.com>
2017-05-22 15:59:23 +0200

    automatically add favicons to entries

--- background/scripts/modules/storage/sm_display.js
+++ background/scripts/modules/storage/sm_display.js
@@ -13,19 +13,37 @@ define(['psl','jquery','scripts/modules/storage/sm_category'],function(psl,$,sm_
 
 			var turl = url.split("/")[2]; // Get the hostname
 			var parsed = psl.parse(turl); // Parse the domain
-			var urlName = parsed.sld;
-
-			var wrapper = $('#entryWrapper_'+credential.id);
-			wrapper.append('<div class="col-lg-2"><a href="http://placehold.it"><img class="placeholder-img" src="http://placehold.it/40x40"></a>'+ urlName +'</div><div class="col-lg-4">'+ url +'</div><div class="col-lg-2">'+ credential.username +'</div><div class="col-lg-4"><div class="row"><div class="col-lg-6">01.01.17</div><div class="col-lg-2"><a class="btn btn-mp light">Edit</a></div><div class="col-lg-2"><a id="'+ url +'" class="btn btn-mp light">Delete</a></div><div class="col-lg-2"><a id="open_'+credential.id+'" class="btn btn-mp light">Goto</a></div></div></div>');
-			//wrapper.append('<div class="row entry"><div class="col-lg-12"><h4>'+url+'</h4><hr><div class="row"><div class="col-lg-8"><p>'+credential.username+'</p></div><div class="col-lg-2 entry-icons"><i id="'+url+'" class="material-icons">delete</i></div><div class="col-lg-2 entry-icons"><i id="open_'+credential.id+'" class="material-icons">open_in_new</i></div></div>');
-			var deleteBtn = document.getElementById(url);
-			var openBtn = document.getElementById("open_"+credential.id);
+			var urlName = parsed.domain;
 			
-			openBtn.addEventListener('click', function(e){
-				var creating = browser.tabs.create({
-					url: url
-				});
-			});
+			var requestURL = "https://icons.better-idea.org/allicons.json?url="+urlName;
+			var wrapper = $('#entryWrapper_'+credential.id);
+
+			var HttpClient = function() {
+				this.get = function(mpURL, mpCallback) {
+					var mpHttpRequest = new XMLHttpRequest();
+					mpHttpRequest.onreadystatechange = function() { 
+						if (mpHttpRequest.readyState == 4 && mpHttpRequest.status == 200)
+							mpCallback(mpHttpRequest.responseText);
+					}
+					mpHttpRequest.open( "GET", mpURL, true );            
+					mpHttpRequest.send( null );
+				}
+			}
+
+			var client = new HttpClient();
+			client.get(requestURL, function(response) {
+    			// console.log(JSON.parse(response).icons[0].url);
+    			var favIcon = JSON.parse(response).icons[0].url;
+    			wrapper.append('<div class="col-lg-2"><a><img class="placeholder-img" src="'+ favIcon +'"></a>'+ urlName +'</div><div class="col-lg-4">'+ url +'</div><div class="col-lg-2">'+ credential.username +'</div><div class="col-lg-4"><div class="row"><div class="col-lg-6">01.01.17</div><div class="col-lg-2"><a class="btn btn-mp light">Edit</a></div><div class="col-lg-2"><a id="'+ url +'" class="btn btn-mp light">Delete</a></div><div class="col-lg-2"><a id="open_'+credential.id+'" class="btn btn-mp light">Goto</a></div></div></div>');
+    		//wrapper.append('<div class="row entry"><div class="col-lg-12"><h4>'+url+'</h4><hr><div class="row"><div class="col-lg-8"><p>'+credential.username+'</p></div><div class="col-lg-2 entry-icons"><i id="'+url+'" class="material-icons">delete</i></div><div class="col-lg-2 entry-icons"><i id="open_'+credential.id+'" class="material-icons">open_in_new</i></div></div>');
+    		var deleteBtn = document.getElementById(url);
+    		var openBtn = document.getElementById("open_"+credential.id);
+    		
+    		openBtn.addEventListener('click', function(e){
+    			var creating = browser.tabs.create({
+    				url: url
+    			});
+    		});
 			//id (== url) is saved in button
 			
 			deleteBtn.addEventListener('click',function(e){
@@ -36,6 +54,11 @@ define(['psl','jquery','scripts/modules/storage/sm_category'],function(psl,$,sm_
 				evtTgt.parentNode.parentNode.parentNode.removeChild(evtTgt.parentNode.parentNode);		
 				
 			});
+
+		});
+
+			
+			
 			
 
 
@@ -59,7 +82,5 @@ define(['psl','jquery','scripts/modules/storage/sm_category'],function(psl,$,sm_
 
 			
 		}
-		
-		
 	}
 });
